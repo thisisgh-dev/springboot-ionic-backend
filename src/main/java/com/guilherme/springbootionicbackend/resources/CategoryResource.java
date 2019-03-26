abf5523e21719f209d1d5d33ee385412436738ec
Guilherme | Web developer <mandeparaguilherme@gmail.com>
2019-03-25 21:53:31 -0300

    Testing the first class domain - Category

--- src/main/java/com/guilherme/springbootionicbackend/resources/CategoryResource.java
+++ src/main/java/com/guilherme/springbootionicbackend/resources/CategoryResource.java
@@ -1,16 +1,28 @@
 package com.guilherme.springbootionicbackend.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.guilherme.springbootionicbackend.domain.Category;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String find() {
-		return "REST ok!";
+	public List<Category> list() {
+
+		Category cat1 = new Category(1, "Informática");
+		Category cat2 = new Category(2, "Escritório");
+		List<Category> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+
+		return list;
 	}
 
 }
