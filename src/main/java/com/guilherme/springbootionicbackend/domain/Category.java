5615089c5b594c8e0a8c86a206f4a811af7698d8
Guilherme | Web developer <mandeparaguilherme@gmail.com>
2019-03-26 01:19:38 -0300

    H2 database and automatic database creation

--- src/main/java/com/guilherme/springbootionicbackend/domain/Category.java
+++ src/main/java/com/guilherme/springbootionicbackend/domain/Category.java
@@ -2,7 +2,6 @@ package com.guilherme.springbootionicbackend.domain;
 
 import java.io.Serializable;
 
-public class Category implements Serializable{
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -28,34 +27,4 @@ public class Category implements Serializable{
 		return true;
 	}
 
-	private static final long serialVersionUID = 1L;
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	private Integer id;
-	private String name;
-	
-	public Category() {		
-	}
-
-	public Category(Integer id, String name) {
-		super();
-		this.id = id;
-		this.name = name;
-	}
-	
 }
