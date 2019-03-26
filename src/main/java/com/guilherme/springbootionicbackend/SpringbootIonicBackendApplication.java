fd11eeba58d7a79c6c7fae8f582ae59d7615a5b0
Guilherme | Web developer <mandeparaguilherme@gmail.com>
2019-03-26 14:28:37 -0300

    Product and association many to many

--- src/main/java/com/guilherme/springbootionicbackend/SpringbootIonicBackendApplication.java
+++ src/main/java/com/guilherme/springbootionicbackend/SpringbootIonicBackendApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.guilherme.springbootionicbackend.domain.Category;
+import com.guilherme.springbootionicbackend.domain.Product;
 import com.guilherme.springbootionicbackend.repositories.CategoryRepository;
+import com.guilherme.springbootionicbackend.repositories.ProductRepository;
 
 @SpringBootApplication
 public class SpringbootIonicBackendApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private ProductRepository productRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootIonicBackendApplication.class, args);
@@ -24,7 +29,21 @@ public class SpringbootIonicBackendApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Category cat1 = new Category(null, "Informatica");
 		Category cat2 = new Category(null, "Escritorio");
+		
+
+		Product P1 = new Product(null, "Computador", 2000.00);
+		Product P2 = new Product(null, "Impressora", 8000.00);
+		Product P3 = new Product(null, "Mouse", 80.00);
+
+		cat1.getProducts().addAll(Arrays.asList(P1, P2, P3));
+		cat2.getProducts().addAll(Arrays.asList(P2));
+
+		P1.getCategories().addAll(Arrays.asList(cat1));
+		P2.getCategories().addAll(Arrays.asList(cat1, cat2));
+		P3.getCategories().addAll(Arrays.asList(cat1));
+		
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+		productRepository.saveAll(Arrays.asList(P1, P2, P3));
 	}
 
 }
--- src/main/java/com/guilherme/springbootionicbackend/domain/Product.java
+++ src/main/java/com/guilherme/springbootionicbackend/domain/Product.java
@@ -8,27 +8,32 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 @Entity
-public class Category implements Serializable {
+public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
-	
-	@ManyToMany(mappedBy="categories")
-	private List<Product> products = new ArrayList<>();
+	private Double price;
 
-	public Category() {
+	@ManyToMany
+	@JoinTable(name = "PRODUCT_CATEGORY", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+	private List<Category> categories = new ArrayList<>();
+
+	public Product() {
 	}
 
-	public Category(Integer id, String name) {
+	public Product(Integer id, String name, Double price) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.price = price;
 	}
 
 	public Integer getId() {
@@ -46,15 +51,23 @@ public class Category implements Serializable {
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	public List<Product> getProducts() {
-		return products;
+
+	public Double getPrice() {
+		return price;
 	}
 
-	public void setProducts(List<Product> products) {
-		this.products = products;
+	public void setPrice(Double price) {
+		this.price = price;
 	}
-	
+
+	public List<Category> getCategories() {
+		return categories;
+	}
+
+	public void setCategories(List<Category> categories) {
+		this.categories = categories;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -71,7 +84,7 @@ public class Category implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Category other = (Category) obj;
+		Product other = (Product) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -79,4 +92,5 @@ public class Category implements Serializable {
 			return false;
 		return true;
 	}
+
 }
