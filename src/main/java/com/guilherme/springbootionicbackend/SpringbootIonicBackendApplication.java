bd452624ca1e6c056a7243bcd9caf75aaa502789
Guilherme | Web developer <mandeparaguilherme@gmail.com>
2019-03-26 02:45:38 -0300

    Creating instantiation operation

--- src/main/java/com/guilherme/springbootionicbackend/SpringbootIonicBackendApplication.java
+++ src/main/java/com/guilherme/springbootionicbackend/SpringbootIonicBackendApplication.java
@@ -1,13 +1,30 @@
 package com.guilherme.springbootionicbackend;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.guilherme.springbootionicbackend.domain.Category;
+import com.guilherme.springbootionicbackend.repositories.CategoryRepository;
+
 @SpringBootApplication
-public class SpringbootIonicBackendApplication {
+public class SpringbootIonicBackendApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootIonicBackendApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Category cat1 = new Category(null, "Informatica");
+		Category cat2 = new Category(null, "Escritorio");
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
